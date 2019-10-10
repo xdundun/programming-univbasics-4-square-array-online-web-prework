3b414fea91bab6d222fc9ed4cf556d45a2e3ab27
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-10 18:49:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,7 @@ def square_array(array)
   counter = 0
   new_numbers = array
   while counter < array.length do
-    new_numbers[case object
-    when condition
-
-    end]
+    new_numbers[counter] = array[counter] **2
     counter += 1
   end
   puts new_numbers
